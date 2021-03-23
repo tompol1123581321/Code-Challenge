8296088ee9e11ba2feba47dfe41950562f62ad26
Tomáš Polívka <tomas@pop-os.localdomain>
2021-03-23 22:20:48 +0100

    first commit

---
+++ src/components/chatWindow.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+import "../style/components.scss"
+const ChatWindow = () => {
+	return (
+		<div className="ChatWindow">
+			<h4>ChatWindow</h4>
+		</div>
+	)
+}
+
+export default ChatWindow
---
+++ src/components/dummyControls.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+import "../style/components.scss"
+const DummyControls = () => {
+	return (
+		<div className="DummyControls">
+			<h4>DummyControls</h4>
+		</div>
+	)
+}
+
+export default DummyControls
